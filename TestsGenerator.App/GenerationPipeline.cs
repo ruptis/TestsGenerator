a8a4389af8a0f2e4da7997d1ea9cb5285f4f98dc
ruptis <90418759+ruptis@users.noreply.github.com>
2024-05-05 19:58:51 +0300

    Parallel constraints

--- TestsGenerator.App/GenerationPipeline.cs
+++ TestsGenerator.App/GenerationPipeline.cs
@@ -8,22 +8,22 @@ public class GenerationPipeline
     private readonly TransformBlock<string, string> _readBlock;
     private readonly ActionBlock<IAsyncEnumerable<TestFile>> _writeBlock;
 
-    public GenerationPipeline(Core.TestsGenerator generator, string outputDirectory, int maxDegreeOfParallelism = 4)
+    public GenerationPipeline(Core.TestsGenerator generator, string outputDirectory, int maxParallelReads = 10, int maxParallelWrites = 10, int maxParallelAnalyzes = 10)
     {
         _outputDirectory = outputDirectory;
         
-        var options = new ExecutionDataflowBlockOptions { MaxDegreeOfParallelism = maxDegreeOfParallelism };
+        var readOptions = new ExecutionDataflowBlockOptions { MaxDegreeOfParallelism = maxParallelReads };
+        var writeOptions = new ExecutionDataflowBlockOptions { MaxDegreeOfParallelism = maxParallelWrites };
+        var analyzeOptions = new ExecutionDataflowBlockOptions { MaxDegreeOfParallelism = maxParallelAnalyzes };
         
-        _readBlock = new TransformBlock<string, string>(async path => await File.ReadAllTextAsync(path), options);
-        _writeBlock = new ActionBlock<IAsyncEnumerable<TestFile>>(async testFiles => await WriteFiles(testFiles), options);
-        var analyzeBlock = new TransformBlock<string, IAsyncEnumerable<TestFile>>(generator.GenerateTests, options);
+        _readBlock = new TransformBlock<string, string>(async path => await File.ReadAllTextAsync(path), readOptions);
+        _writeBlock = new ActionBlock<IAsyncEnumerable<TestFile>>(async testFiles => await WriteFiles(testFiles), writeOptions);
+        var analyzeBlock = new TransformBlock<string, IAsyncEnumerable<TestFile>>(generator.GenerateTests, analyzeOptions);
 
         var linkOptions = new DataflowLinkOptions { PropagateCompletion = true };
         
         _readBlock.LinkTo(analyzeBlock, linkOptions);
-        _readBlock.Completion.ContinueWith(_ => analyzeBlock.Complete());
         analyzeBlock.LinkTo(_writeBlock, linkOptions);
-        analyzeBlock.Completion.ContinueWith(_ => _writeBlock.Complete());
     }
     
     public async Task GenerateTests(IEnumerable<string> paths)
--- TestsGenerator.App/Program.cs
+++ TestsGenerator.App/Program.cs
@@ -7,13 +7,38 @@ if (args.Length < 2)
     return;
 }
 
-var files = args[..^1]
+var maxParallelReadsString = TryGetArgString(args, "--maxParallelReads=");
+var maxParallelAnalyzesString = TryGetArgString(args, "--maxParallelAnalyzes=");
+var maxParallelWritesString = TryGetArgString(args, "--maxParallelWrites=");
+
+var maxParallelReads = maxParallelReadsString != null ? int.Parse(maxParallelReadsString) : 10;
+var maxParallelAnalyzes = maxParallelAnalyzesString != null ? int.Parse(maxParallelAnalyzesString) : 10;
+var maxParallelWrites = maxParallelWritesString != null ? int.Parse(maxParallelWritesString) : 10;
+
+var firstOptionIndex = args.FirstOrDefault(arg => arg.StartsWith("--")) is { } option
+    ? Array.IndexOf(args, option)
+    : args.Length;
+
+var files = args[..(firstOptionIndex-1)]
     .SelectMany(path => Directory.Exists(path)
         ? Directory.GetFiles(path, "*.cs", SearchOption.AllDirectories)
         : [path])
     .ToArray();
 
-var outputDirectory = args[^1];
+var outputDirectory = args[firstOptionIndex-1];
 
-var pipeline = new GenerationPipeline(TestsGeneratorsFactory.CreateNUnitTestsGenerator(), outputDirectory);
+var pipeline = new GenerationPipeline(
+    TestsGeneratorsFactory.CreateNUnitTestsGenerator(),
+    outputDirectory,
+    maxParallelReads,
+    maxParallelWrites,
+    maxParallelAnalyzes);
+
 await pipeline.GenerateTests(files);
+return;
+
+static string? TryGetArgString(IEnumerable<string> args, string prefix)
+{
+    var arg = args.FirstOrDefault(arg => arg.StartsWith(prefix));
+    return arg?[prefix.Length..];
+}
